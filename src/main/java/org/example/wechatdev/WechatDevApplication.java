5ec9737895ea1b692ec0e7235eaf24e268fb5cff
74705 <747057591@qq.com>
2025-09-18 15:46:27 +0800

    初始工程

---
+++ src/main/java/org/example/wechatdev/WechatDevApplication.java
@@ -0,0 +1,13 @@
+package org.example.wechatdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WechatDevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/wechatdev/WechatDevApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.wechatdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WechatDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
